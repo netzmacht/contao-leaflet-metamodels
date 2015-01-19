c054226af9cd8a450e76a6aadc675a2375f123fd
David Molineus <david.molineus@netzmacht.de>
2015-01-19 14:25:04 +0100

    Update translations.

--- module/languages/en/modules.php
+++ module/languages/en/modules.php
@@ -1,4 +1,4 @@
 <?php
 
-$GLOBALS['TL_LANG']['MOD']['leaflet-metamodels'][0] = 'Leaflet MetaModel layer';
+$GLOBALS['TL_LANG']['MOD']['leaflet-metamodels'][0] = 'Leaflet MetaModels layer';
 $GLOBALS['TL_LANG']['MOD']['leaflet-metamodels'][1] = 'A Leaflet layer integrating data from MetaModels';
