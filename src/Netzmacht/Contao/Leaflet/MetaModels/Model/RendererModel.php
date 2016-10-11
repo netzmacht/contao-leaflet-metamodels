6910d9f1d6d15ce3197078d2a2905716390e8ecf
David Molineus <david.molineus@netzmacht.de>
2016-10-11 10:02:13 +0200

    Update file headers.

--- src/Netzmacht/Contao/Leaflet/MetaModels/Model/RendererModel.php
+++ src/Netzmacht/Contao/Leaflet/MetaModels/Model/RendererModel.php
@@ -1,9 +1,9 @@
 <?php
 
 /**
- * @package    dev
+ * @package    contao-leaflet-metamodels
  * @author     David Molineus <david.molineus@netzmacht.de>
- * @copyright  2015 netzmacht creative David Molineus
+ * @copyright  2015-2016 netzmacht David Molineus
  * @license    LGPL 3.0
  * @filesource
  *
